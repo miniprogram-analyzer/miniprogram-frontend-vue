c6a1fa965024a31f63d436869084a8be921d1221
Joanna-0421 <zhangziy0421@163.com>
2020-07-07 22:33:10 +0800

    Enter

--- src/views/dashboard/admin/components/TransactionTable.vue
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -1,7 +1,11 @@
 <template>
   <el-card class="box-card">
   <div>
-    <a href="https://github.com/PanJiaChen/vue-element-admin/blob/master/README.zh-CN.md">源码地址</a>
+    <el-popover placement="right" width="400" trigger="click">
+      原因：这往往是index.js里面Page的P没有大写导致的，也有可能是app.json文件里面页面路径配置不对
+      解决方法：1.检查一下index.js文件中Page里面的P是否大写了。2.检查一下app.json文件里面的路径配置
+      <el-button type="text" slot="reference">报错类型：page is not defined </el-button>
+    </el-popover>
   </div>
  </el-card>
 </template>
